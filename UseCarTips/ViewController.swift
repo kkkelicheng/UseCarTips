6c74f212cdcf63f3c91109692497b0b5378a4bd1
kelicheng <960447498@qq.com>
2022-06-30 10:26:28 +0800

    first init

---
+++ UseCarTips/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UseCarTips
+//
+//  Created by kkkelicheng on 2022/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
